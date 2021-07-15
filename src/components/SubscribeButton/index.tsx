d6d80e7a683b4e4d4296e85763ae3ffb1e404190
Caique Degaspari <caique.degaspare@gmail.com>
2021-07-15 18:24:40 -0300

    first commit

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.scss'
+
+interface SubscribeButonProps {
+  priceId:string
+}
+
+export function SubscribeButton({priceId}:SubscribeButonProps) {
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+    >
+      Subscribe now
+    </button>
+  )
+}
